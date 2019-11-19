30425aeefed99b687f97d5ecff68f44c952752b9
Varun Resh Maddali <mvresh@gmail.com>
2019-11-19 10:08:58 +0530

    test commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:day_33_morning_challenge_curve/main.dart';
 
 void main() {
+  //tests for rahul
   Curve myCurve = Curves.ease;
   print(myCurve.transform(0.5));
   test("Testing curve value", (){
